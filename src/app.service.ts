4e2c5d1c97667a6c4be724c0fcc26dca9f9929c0
ElidrissiAnass <elidrissianas6@gmail.com>
2023-12-25 11:07:43 +0100

    tehee

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return ['Hello Wissal <3'];
+    return ['Hello Wissal <3 !!'];
   }
 }
