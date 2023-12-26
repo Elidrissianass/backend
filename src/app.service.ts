def75b92e209ecbbf1391c72b5039bcdc7c4e455
ElidrissiAnass <elidrissianas6@gmail.com>
2023-12-26 10:29:11 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return ['Hello DEV WORLD UPDATED <3 !!'];
+    return ['Hello DEV WORLD UPDATED LAST OPERATION <3 !!'];
   }
 }
